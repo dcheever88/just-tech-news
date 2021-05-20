10f2a22d3de36f31335138132425f683be3434f3
dcheever <cheeverdanny@gmail.com>
2021-05-19 21:24:13 -0500

    complete upvote routes and clean code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
     Post.findAll({
-        attributes: ["id", "post_url", "title", "created_at"],
+        attributes: ["id", "post_url", "title", "created_at",
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
         order: [["created_at", "DESC"]],
         include: [
             {
@@ -25,7 +28,9 @@ router.get("/:id", (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ["id", "post_url", "title", "created_at"],
+        attributes: ["id", "post_url", "title", "created_at",
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
         include: [
             {
                 model: User,
@@ -35,7 +40,7 @@ router.get("/:id", (req, res) => {
     })
         .then(dbPostData => {
             if (!dbPostData) {
-                res.status(400).json( { message: "No post found with this id" });
+                res.status(404).json( { message: "No post found with this id" });
                 return;
             }
             res.json(dbPostData);
@@ -57,6 +62,17 @@ router.post("/", (req, res) => {
             console.log(err);
             res.json(500).json(err);
         });
+});
+
+// PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
